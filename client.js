f6822cbba4673298b9ee3aae61665e64321f1769
Benji <thebkrcanbefound@gmail.com>
2017-11-10 18:10:15 -0800

    updates to handle errors with get and post. still no db integration

--- client.js
+++ client.js
@@ -4,7 +4,9 @@ var notepad = angular.module('notepad', []);  //'ngRoute'
 
 //first time page opens should GET all notes
   //Change to be a button to refresh later.
-function mainController($scope, $http) {
+// function mainController($scope, $http) {
+notepad.controller('mainController', ['$scope', '$http', function($scope, $http) {
+
 
   //get request to server for all notes
   // $http({
@@ -12,41 +14,58 @@ function mainController($scope, $http) {
   //   url: '/note'
   // })
         //or
-  $http.get('/note', config).then(success, error) {
+  $http.get('/note').then(function(response, error){
     if (error) {
       console.log('error GET line 17 client', error)
     }
-    if (success) {
-      console.log('SUCCESS GET line 20 client', success);
+    if (response) {
+      console.log('SUCCESS GET line 20 client', response);
       //$scope.todos = succes .data?
     }
-  }
+  }, function(response, error){
+    if (error) {
+      console.log('error GET line 25 client', error)
+    }
+    if (response) {
+      console.log('SUCCESS GET line 28 client', response);
+      //$scope.todos = succes .data?
+    }
+  });
+
 
   //post request to server.
     //pass the inputted text (as json?)
   $scope.textToPost = {};
 
   $scope.addNote = function() {
-    $http.post('/note', $scope.textToPost, config).then(success, error) {
+    $http.post('/note', $scope.textToPost).then(function(response, error){
     if (error) {
-      console.log('error POST line 31 client', error)
+      console.log('error POST line 41 client', error)
     }
-    if (success) {
-      console.log('SUCCESS POST line 34 client', success);
+    if (response) {
+      console.log('SUCCESS POST line 44 client', response);
       //$scope.todos = succes .data?
     }
-  };
-
-
-
-}
-
-
-
-
-notepad.controller('myCtrl', function($scope) {
-  $scope.products = ['walk the unicorn', 'feed the blue whale'];
-  $scope.addNote = function(item) {
-    $scope.products.push($scope.newNote);
+  }, function(response, error){
+    if (error) {
+      console.log('error POST line 49 client', error)
+    }
+    if (response) {
+      console.log('SUCCESS POST line 52 client', response);
+      //$scope.todos = succes .data?
+    }
+  });
   }
-});
+
+}]);
+
+
+
+
+
+// notepad.controller('myCtrl', function($scope) {
+//   $scope.products = ['walk the unicorn', 'feed the blue whale'];
+//   $scope.addNote = function(item) {
+//     $scope.products.push($scope.newNote);
+//   }
+// });
